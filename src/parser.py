de9bf899abda97cf2dbd60cb532bb688b6bbcd7a
McKenna <mckennak@ksu.edu>
2015-10-29 09:31:44 -0500

    Added a new dictionary (alt12dicts). Support for swapping between dictionaries to be added later. Added a few phrases to the parser.

--- src/parser.py
+++ src/parser.py
@@ -1,12 +1,41 @@
 class Parser:
 
     def parse_tokens( self, tokens ):
+        #phrases of length 1
+        if len( tokens ) == 1:
+            # N - 'fireman'
+            if 'N' in tokens[ 0 ]:
+                return True
+   
+            # V or r - 'run'
+            elif 'V' in tokens[ 0 ] or 'R' in tokens[ 0 ]:
+                return True
+
         #phrases of length 2
-        if len( tokens ) == 2:
+        elif len( tokens ) == 2:
             if 'A' in tokens[ 0 ]:
-                # A,N
+                # A,N - 'good dog'
                 if 'N' in tokens[ 1 ]:
                     return True
+
+                # A,V or A,r - 'good run'
+                elif 'V' in tokens[ 1 ] or 'R' in tokens[ 1 ]:
+                    return True
+
+            elif 'v' in tokens[ 0 ]:
+                # v,V or a,r - 'quickly run'
+                if 'V' in tokens[ 1 ] or 'R' in tokens[ 1 ]:
+                    return True
+
+                # v,s - 'quickly ran'
+                elif 's' in tokens[ 1 ]:
+                    return True
+
+                # v,t - 'quickly spoken'
+                #elif 't' in tokens[ 1 ]:
+                    #return True
+
+            
 
         return False
 
--- src/phrase_unscrambler.py
+++ src/phrase_unscrambler.py
@@ -16,6 +16,7 @@ def iterate_phrases( phrases, lexer, parser, cur=[] ):
         #phrase is finished
         else:
             tokens = lexer.scan_phrases( cur )
+            print tokens
             all_tokens = list( permutations( tokens ) )
             all_phrases = list( permutations( cur ) )
             for i, tokens in enumerate( all_tokens ):
@@ -23,30 +24,72 @@ def iterate_phrases( phrases, lexer, parser, cur=[] ):
                     print all_phrases[ i ]
             del cur[ -1 ]
 
+def read_dict( file, trie ):
+    for line in file:
+        #these words are uncommon (see README-infl)
+        if line[ 0 ].isalpha():
+            #split on whitespace
+            line = line.split()
+            if len( line[ 0 ] ) < 13:
+	        if line[1][0] == 'A' and line[0].endswith( 'ly' ):
+		    trie.add_word( line[0].lower(), 'v' )
+                else:
+                    trie.add_word( line[0].lower(), line[1][0] )
+            
+            for word in line[ 2: ]:
+                #throw out non-alpha words (see README-infl)
+                if word.isalpha() and len( word ) < 13:
+                    if line[1][0] == 'V':
+                        #simple present
+                        if word.endswith('s'):
+                            trie.add_word( word, 'R' )
+                        #simple past
+                        elif word.endswith('id') or word.endswith('ed'):
+                            trie.add_word( word, 's' )
+                        #present participle
+                        elif word.endswith('ing'):
+                            trie.add_word( word, 'T' )
+                        #past participle
+                        elif word.endswith('en'):
+                            trie.add_word( word, 't' )
+                        else:
+                            print word
+                    elif line[1][0] == 'N':
+                        #plural
+                        trie.add_word( word, 'P' )
+                    elif line[1][0] == 'A':
+                        trie.add_word( word, 'A' )
 
 
 trie = Trie()
 #populate trie
-f = open( '../mpos/mobypos.txt', 'r' )
+#f = open( '../mpos/mobypos.txt', 'r' )
+f = open( '../alt12dicts/2of12id.txt', 'r' )
 
-for line in f:
-    if len( line ) < 13: 
-        line = line.rstrip()
-        word = line[ : line.index('\\') ]
-        trie.add_word( word.lower(), line[ line.index('\\') + 1 : ] )
+read_dict( f, trie )
+
+#for line in f:
+    #if line[ 0 ] != '-':
+        #line = line.rstrip()
+        #word = line[ : line.index('\\') ]
+        #word = line[ : line.index(' ') ]
+        #if len( word ) < 13:
+            #print word
+            #trie.add_word( word.lower(), line[ line.index('\\') + 1 : ] )
+            #trie.add_word( word.lower(), line[ line.index(' ') + 1 ] )
 
 #inp = raw_input( "Enter scrambled characters: " )
 #inp = inp.lower()
 
 
-words = trie.build_words( 'fearthegoat', trie.root )
-#words = trie.build_words( 'dgdgooo', trie.root )
+#words = trie.build_words( 'fearthegoat', trie.root )
+words = trie.build_words( 'dgdgooo', trie.root )
 print words
 print "\n\n\n"
 
 
-phrases,f = trie.build_phrases( words, 'fearthegoat' )
-#phrases,f = trie.build_phrases( words, 'dgdgooo' )
+#phrases,f = trie.build_phrases( words, 'fearthegoat' )
+phrases,f = trie.build_phrases( words, 'dgdgooo' )
 print phrases
 print "\n\n\n"
 
